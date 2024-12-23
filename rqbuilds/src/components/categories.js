50f5097394ae4cf1d4de761013d7485fac38ccb4
Brian Nguyen <briannguyen636@gmail.com>
2024-12-23 15:59:42 -0800

    fixed rerender issues

--- rqbuilds/src/components/categories.js
+++ rqbuilds/src/components/categories.js
@@ -1,35 +1,50 @@
 import React, {useState} from "react"
 import Category from "./category";
 import './categories.css'
+import { PERKS as perklist } from '../data/perkdata';
+
 
 export default function Categories(props) {
 
     const [categories, setCategories] = useState([
         {
-            id: 1,
+            id: 0,
             label: "Category 1",
             perks:[],
             classID: props.classID
         }
     ]);
 
+    function dragOver(e) {        
+        e.preventDefault();
+    }
+    function handleDrop(e) {    
+        const data = e.dataTransfer.getData("text/plain")
+        const object = perklist[props.classID][data]
+        if (e.target.className == "categoryGrid") {
+            let category = categories[e.target.id]
+            if (!category.perks.includes(object)) category.perks = [...category.perks, object]
+            setCategories([...categories])
+        }
+    }
+    
+
     return(
         <div>
             <div id='categories'>
                 {categories.map(e => {
                 return(
-                    <div>
+                    <div onDragOver={e => dragOver(e)} onDrop={e=>handleDrop(e)}>
                         {Category(e)}
                     </div>
-                )
-                })}
+                )})}
             </div>
 
             <button className="btn btn-primary" disabled={categories.length >= 5} onClick={()=>{
                 let newId = categories.length + 1
                 setCategories([...categories, {
-                    id: newId,
-                    label: "Category " + newId,
+                    id: newId - 1,
+                    label: "Category " + newId ,
                     perks:[],
                     classID: props.classID
                 }])
--- rqbuilds/src/components/category.js
+++ rqbuilds/src/components/category.js
@@ -1,30 +1,17 @@
 import React, {Component, useState} from 'react';
 import './category.css'
 import perk from './perk';
-import { PERKS as perklist } from '../data/perkdata';
 
 
 export default function Category(props) {
-
-    const [perks, setPerks] = useState(props.perks)
-
-    function dragOver(e) {        
-        e.preventDefault();
-    }
-    function handleDrop(e) {    
-        const data = e.dataTransfer.getData("text/plain")
-        const object = perklist[props.classID][data]
-        console.log(object)
-        setPerks([...perks, object])
-    }
 
     return (
         <div id='category'>
             <button id='categoryLabel' className='btn'>
                 {props.label}
             </button>
-            <div className='categoryGrid' onDragOver={e => dragOver(e)} onDrop={e=>handleDrop(e)}>
-                {perks.map(e => {return(
+            <div className='categoryGrid' id={props.id}>
+                {props.perks.map(e => {return(
                     <div id={e.name} key={e.id}>
                         {perk(e)}
                     </div>
